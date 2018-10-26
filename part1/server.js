59fa95dd5a72323e2460ba6ca49b4742ffd59842
HarrisonVanNorth <hgvannort@gmail.com>
2018-10-26 12:22:51 -0700

    final

--- part1/server.js
+++ part1/server.js
@@ -6,6 +6,38 @@ app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
 
+app.get('/hello', function(req, res){
+  console.log('get request')
+  res.send('Hello!');
+});
+
+app.post('/create/:cat', function(req, res){
+ let dog = {
+    id :1,
+     cat : req.params.cat
+    }
+    res.json(dog);
+});
+
+app.get('/', function(req, res){
+  res.sendFile('/Users/dhutc/Documents/Galvanize/Q2/expressIntroChallenges/part1/index.html', function(err){
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('file sent')
+    }
+  });
+});
+
+app.get('/verify/:age', function(req, res){
+  let thing = req.params.age
+  if (thing >= 13){
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(403)
+  }
+});
+
 app.use(function(req, res) {
   res.sendStatus(404);
 });
--- part2/server.js
+++ part2/server.js
@@ -1,9 +1,52 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 8000;
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
+});
+
+//challenge 1
+
+app.post('/create/:name/:age', function(req, res){
+  let user = {
+    name: req.params.name,
+    age: parseInt(req.params.age)
+  }
+
+  let raw = fs.readFileSync('./storage.json','utf8')
+  let content;
+  if (raw === ''){
+    content = [];
+  } else {
+  content = JSON.parse(raw)
+  }
+  content.push(user);
+  console.log(content);
+  fs.writeFileSync('./storage.json', JSON.stringify(content))
+  res.send('done')
+});
+
+//challenge 2
+
+app.get('/', (req, res)=>{
+  let data = fs.readFileSync('./storage.json', 'utf8');
+  console.log(data)
+  res.send(data)
+});
+
+//challenge 3
+
+app.get('/:name', (req,res)=>{
+  let data = fs.readFileSync('./storage.json', 'utf8');
+  let content = JSON.parse(data);
+  let result = content.filter(item => item.name === req.params.name)[0];
+  if (result){
+    res.json(result)
+  } else {
+    res.sendStatus(400)
+  }
 });
 
 app.use(function(req, res) {
@@ -13,3 +56,4 @@ app.use(function(req, res) {
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
+
